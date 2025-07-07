37acffa70c9eed13f90d5d73ff4168dd7c744fe5
Marvinx9 <afraniodantas0224@gmail.com>
2025-07-07 13:19:45 -0300

    chore: add swagger configuration

--- src/main/java/com/marvin/store/StoreApplication.java
+++ src/main/java/com/marvin/store/StoreApplication.java
@@ -1,9 +1,12 @@
 package com.marvin.store;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Store API", version = "1.0", description = "Api from manager store"))
 public class StoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
--- src/main/java/com/marvin/store/resources/UserResource.java
+++ src/main/java/com/marvin/store/resources/UserResource.java
@@ -2,8 +2,15 @@ package com.marvin.store.resources;
 
 import com.marvin.store.entities.User;
 import com.marvin.store.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -18,6 +25,10 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
+    @Operation(summary = "Lista todos os usuários")
+    @ApiResponse(responseCode = "200", description = "lista de usuários retornado com sucesso", content = {
+            @Content(mediaType = "application/json",
+            array = @ArraySchema(schema = @Schema(implementation = User.class)))})
     public ResponseEntity<List<User>> findAll(){
         List<User> list = userService.findAll();
 
@@ -25,12 +36,27 @@ public class UserResource {
     }
 
     @GetMapping(value = "/{id}")
+    @Operation(summary = "Recebe o id do usuário e busca no banco de dados")
+    @ApiResponse(responseCode = "200", description = "usuario retornado com sucesso", content = {
+            @Content(mediaType = "application/json",
+            schema = @Schema(implementation = User.class))
+    })
     public ResponseEntity<User> findById(@PathVariable Long id){
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
     }
 
     @PostMapping
+    @Operation(summary = "Cria um usuário no banco de dados")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Usuário criado com sucesso", content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class))}
+            ),
+            @ApiResponse(responseCode = "500", description = "Ocorreu um erro ao salvar esse usuário", content = {
+                    @Content(schema = @Schema(implementation = Void.class))
+            })
+    })
     public ResponseEntity<User> insert(@RequestBody User obj) {
         obj = userService.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
